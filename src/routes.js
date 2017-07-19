2cbd1bfc90b500ea2b8d968e422bd22d39635a1d
Bo Andersen <ba0708@icloud.com>
2017-07-19 16:06:40 +0200

    Added redirect rule

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 import ProductList from './ProductList.vue';
-import Cart from './Cart.vue';
 import ViewProduct from './ViewProduct.vue';
+import Cart from './Cart.vue';
 
 export const routes = [
+    // LEGACY ROUTES
+    { path: '/products/:productId/view', redirect: { name: 'viewProduct' } },
+    // /LEGACY ROUTES
+    
     { path: '', component: ProductList },
-    { path: '/cart', component: Cart },
     { path: '/products/:productId', name: 'viewProduct', props: true, component: ViewProduct },
+    { path: '/cart', component: Cart },
     { path: '*', component: { template: '<h1>Page Not Found!</h1>' } }
 ];
