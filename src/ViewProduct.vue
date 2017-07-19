6250ab6a7ee00f9f015a6f1ae4dca1f1063d8b53
Bo Andersen <ba0708@icloud.com>
2017-07-19 15:28:04 +0200

    Added watcher for route parameter

--- src/ViewProduct.vue
+++ src/ViewProduct.vue
@@ -5,6 +5,17 @@
         <p><strong>Price:</strong> {{ product.price | currency }}</p>
         <p><strong>In stock:</strong> {{ product.inStock }}</p>
         <p>{{ product.description }}</p>
+
+        <div v-if="relatedProducts != null">
+            <h2>Related Products</h2>
+            <ul>
+                <li v-for="related in relatedProducts">
+                    <router-link :to="{ name: 'viewProduct', params: { productId: related.id } }">
+                        {{ related.name }}
+                    </router-link>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -26,6 +37,11 @@
         created() {
             this.product = this.getProduct(this.productId);
         },
+        watch: {
+            productId(newValue, oldValue) {
+                this.product = this.getProduct(newValue);
+            }
+        },
         methods: {
             getProduct(productId) {
                 let match = null;
@@ -37,6 +53,25 @@
                 });
 
                 return match;
+            }
+        },
+        computed: {
+            relatedProducts() {
+                if (this.product === null) {
+                    return [];
+                }
+
+                let related = [];
+                let count = 0;
+
+                this.products.forEach((product) => {
+                    if (product.id != this.product.id && count < 5) {
+                        related.push(product);
+                        count++;
+                    }
+                });
+
+                return related;
             }
         }
     }
