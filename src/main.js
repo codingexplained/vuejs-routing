1389f01f01aef09c2623d4c872d52ea48eaf459d
Bo Andersen <ba0708@icloud.com>
2017-07-19 14:51:04 +0200

    Added routes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router';
 import App from './App.vue'
+import ProductList from './ProductList.vue';
+import Cart from './Cart.vue';
 
 Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes: [
+        { path: '', component: ProductList },
+        { path: '/cart', component: Cart }
+    ]
+});
 
 Vue.filter('currency', function(value) {
     let formatter = new Intl.NumberFormat('en-US', {
@@ -17,6 +26,7 @@ Vue.filter('currency', function(value) {
 export const eventBus = new Vue();
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    render: h => h(App),
+    router: router
 })
