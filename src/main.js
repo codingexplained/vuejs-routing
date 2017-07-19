e2df58c6ab5de5512131d082f4489bbc6d4e47a8
Bo Andersen <ba0708@icloud.com>
2017-07-19 18:24:12 +0200

    Added scroll behavior

--- src/main.js
+++ src/main.js
@@ -7,7 +7,20 @@ Vue.use(VueRouter);
 
 const router = new VueRouter({
     routes: routes,
-    mode: 'history'
+    mode: 'history',
+    scrollBehavior(to, from, savedPosition) {
+        if (to.hash) {
+            return {
+                selector: to.hash
+            };
+        }
+        
+        if (savedPosition) {
+            return savedPosition;
+        }
+        
+        return { x: 0, y: 0 };
+    }
 });
 
 Vue.filter('currency', function(value) {
