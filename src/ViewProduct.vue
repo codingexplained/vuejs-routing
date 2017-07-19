c1a01345f5c555c9374963330f2e8c81424bb625
Bo Andersen <ba0708@icloud.com>
2017-07-19 15:22:08 +0200

    Retrieved route parameter

--- src/ViewProduct.vue
+++ src/ViewProduct.vue
@@ -1,9 +1,39 @@
 <template>
-    <h1>Viewing a product</h1>
+    <div>
+        <h1>{{ product.name }}</h1>
+        <p><strong>ID:</strong> {{ product.id }}</p>
+        <p><strong>Price:</strong> {{ product.price | currency }}</p>
+        <p><strong>In stock:</strong> {{ product.inStock }}</p>
+        <p>{{ product.description }}</p>
+    </div>
 </template>
 
 <script>
-    export default {
+    import { products } from './data/products';
 
+    export default {
+        data() {
+            return {
+                products: products,
+                product: null
+            };
+        },
+        created() {
+            let productId = this.$route.params.productId;
+            this.product = this.getProduct(productId);
+        },
+        methods: {
+            getProduct(productId) {
+                let match = null;
+
+                this.products.forEach(function(product) {
+                    if (product.id == productId) {
+                        match = product;
+                    }
+                });
+
+                return match;
+            }
+        }
     }
 </script>
